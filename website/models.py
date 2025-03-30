36704fcb8bff56bcf4b2f6752036dc9eb84c44cc
Daryna Horetska <daryna.gorecka@gmail.com>
2025-03-30 17:18:17 +0300

    add profile to templates

--- website/models.py
+++ website/models.py
@@ -11,8 +11,12 @@ class Note(db.Model):
 
 
 class User(db.Model, UserMixin):
+    class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(150), unique=True)
     password = db.Column(db.String(150))
     first_name = db.Column(db.String(150))
+    last_name = db.Column(db.String(150))
+    profession = db.Column(db.String(150))
+    date_created = db.Column(db.DateTime(timezone=True), default=func.now())
     notes = db.relationship('Note')
