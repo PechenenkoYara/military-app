5fc12f4a73b43be0f10b2372f0da6fe6cc9f43b2
Yaryna Pechenenko <pechenenko.pn@ucu.edu.ua>
2025-03-30 19:59:38 +0300

    one day

--- website/models.py
+++ website/models.py
@@ -6,17 +6,16 @@ from sqlalchemy.sql import func
 class Note(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     data = db.Column(db.String(10000))
-    date = db.Column(db.DateTime(timezone=True), default=func.now())
+    date = db.Column(db.DateTime(timezone=True), default=func.now)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
 
 class User(db.Model, UserMixin):
-    class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(150), unique=True)
     password = db.Column(db.String(150))
     first_name = db.Column(db.String(150))
     last_name = db.Column(db.String(150))
     profession = db.Column(db.String(150))
-    date_created = db.Column(db.DateTime(timezone=True), default=func.now())
+    date_created = db.Column(db.DateTime(timezone=True), default=func.now)
     notes = db.relationship('Note')
