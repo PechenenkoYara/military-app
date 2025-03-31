f869f2746f9e79ba4961125eb8d07aeb2f9962da
Daryna Horetska <daryna.gorecka@gmail.com>
2025-03-31 20:16:39 +0300

    preventing conflict

--- website/models.py
+++ website/models.py
@@ -11,7 +11,6 @@ class Note(db.Model):
 
 
 class User(db.Model, UserMixin):
-    class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(150), unique=True)
     password = db.Column(db.String(150))
