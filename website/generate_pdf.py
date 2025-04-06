cfaa80e604a6ab2a036ea1c98b8a06350f135830
Daryna Horetska <daryna.gorecka@gmail.com>
2025-04-06 12:34:15 +0300

    add sell before input text2

--- website/generate_pdf.py
+++ website/generate_pdf.py
@@ -22,8 +22,8 @@ def create_pdf():
     pdf.add_page()
     pdf.set_font("Arial", size=12)
 
-    pdf.cell(0, 10, 'Звіт:', ln=True)
-    pdf.cell(0, 10, 'Необхідний equipment', ln=True)
+    pdf.cell(0, 10, 'Smt:', ln=True)
+    pdf.cell(0, 10, 'equipment', ln=True)
     pdf.ln(10)
 
     for line in text.split('\n'):
