8dac834be091f66998ee5e16224cc2762f24bab1
Anastasiya Shopska <shopska.pn@ucu.edu.ua>
2025-04-13 15:21:48 +0300

    added funcion for validation password, addede documentation and combined two functions to validate user's name

--- website/data_checking.py
+++ website/data_checking.py
@@ -1,15 +1,15 @@
 """file to with functions to check, wether the user's input is valid or no"""
 def is_valid_email(email: str) -> bool:
-    """checks if the given email is valid"""
+    """checks if the given email is valid.
+    It is valid if there is "@" in it, also it should contain a domain, which is 
+    being cheked as well.
+    """
     if '@' not in email or email.count('@') != 1:
         return False
 
     local_part, domain_part = email.split('@')
 
     if not local_part or local_part[0] == '.' or local_part[-1] == '.':
-        return False
-
-    if '.' not in domain_part:
         return False
 
     if domain_part[0] == '.' or domain_part[-1] == '.':
@@ -22,7 +22,9 @@ def is_valid_email(email: str) -> bool:
     return True
 
 def is_valid_phone_number(number):
-    """Checks if the given phone number is valid"""
+    """Checks if the given phone number is valid. The phone number is valid
+    if it's lenght is propper and if there are only allowed symbols in it.
+    """
     if not (number.startswith("+") or number.startswith("0")):
         return False
     if len(number) < 10 or len(number) > 15:
@@ -37,46 +39,29 @@ def is_valid_phone_number(number):
             return False
     return True
 
-def is_valid_name(name: str) -> bool:
-    """Checks if the given name is valid"""
+
+def is_name_valid(name: str) -> bool:
+    """Function to validate given person name (first or last).
+    It checks, wether the first letter of the name is upper, the lenght, then 
+    if separators forund in the name, it checks, if they are being used correctly without it's 
+    repeating and if the first letter after separator is upper.
+    """
+    max_length = 150
+    if not name or len(name) > max_length:
+        return False
     if name[0].islower():
-        return False
-    if len(name) == 1 and name.isupper():
-        return True
-    
-    if not name or len(name) > 150:
-        return False
-
-    allowed_separators = "-' "
-    for ch in name:
-        if not (ch.isalpha() or ch in allowed_separators):
-            return False
-
-    parts = name.replace("-", " ").replace("'", " ").split()
-    for part in parts:
-        if len(part) < 2:
-            return False
-    
-    return True
-
-
-def is_valid_last_name(last_name: str) -> bool:
-    """Checks if the given last name is valid"""
-    if last_name[0].islower():
-        return False
-    if not last_name or len(last_name) > 50:
         return False
 
     allowed_separators = "-' "
     prev_char = ""
-    for ch in last_name:
+    for ch in name:
         if not (ch.isalpha() or ch in allowed_separators):
             return False
         if ch in allowed_separators and prev_char in allowed_separators:
             return False
         prev_char = ch
 
-    parts = last_name.replace("-", " ").replace("'", " ").split()
+    parts = name.replace("-", " ").replace("'", " ").split()
     for part in parts:
         if len(part) == 1:
             if not part.isupper():
@@ -85,3 +70,37 @@ def is_valid_last_name(last_name: str) -> bool:
             return False
 
     return True
+
+def is_password_incorrect(password: str):
+    """function to meke sure user's password is valid.
+    It checks if the password is longer, than 7 characters,
+    if it has at least one upper case letter and one sybmol, which is not letter or digit.
+    if the password is valid function returns False
+    """
+    special_characters = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"
+
+    if len(password) < 7:
+        return "Password must contain at least 8 characters"
+
+    if ' ' in password:
+        return "There can be no spaces in password"
+
+    if not any(ch.isdigit() for ch in password):
+        return "Password must include at least one digit"
+
+    if not any(ch.isalpha() for ch in password):
+        return "Password must include at least one letter"
+
+    if not any(ch.islower() for ch in password):
+        return "Password must include at least one lowercase letter"
+
+    if not any(ch.isupper() for ch in password):
+        return "Password must include at least one uppercase letter"
+
+    if not any(ch in special_characters for ch in password):
+        return "Password must include at least one special character"
+
+    if not all(ch.isalnum() or ch in special_characters for ch in password):
+        return "Password can only contain letters, digits and common symbols"
+
+    return False
