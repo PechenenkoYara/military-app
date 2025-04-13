8bb16c5e37905d6638f6e04702ebb5fc1082f1a9
Suchok Kateryna <katsu4ok@gmail.com>
2025-04-13 18:49:12 +0300

    fixed buttons and filter

--- website/contact_filter.py
+++ website/contact_filter.py
@@ -13,15 +13,6 @@ CATEGORIES = {
 
 def filter_input(users_input:str) -> list:
     """Function to filter contacts by their seprciality"""
-    CATEGORIES = {
-        "Автомобілі": ["водій", "механік", "технік", "машин", "автомобіл", 'фур', 'вантажівк', 'шин'],
-        "Медицина та медичне обладнання": ["лік", 'турнік', 'бинт', 'апечк', 'медицин', 'медик'],  #added some key words
-        "Одяг і амуніція": ["одяг", "амунц", "брон", "штан", "шкарпетк", 'термобілизн', 'куртк'],
-        "Сітки": ["сітк"],
-        "3D-друк": ["друк", 'пластик', 'принтер', 'дизайн'],
-        "Кошти":['гроші', 'збор', 'збір', 'кошти'],
-        "Продукти":['м\'ясо', 'продукт', 'риб', 'овоч', 'молочн', 'молочк']
-    }
 
     result = set()
     words = users_input.lower().split()
@@ -36,7 +27,7 @@ def filter_input(users_input:str) -> list:
     return list(result) if result else ['Інше']
 
 def does_fit(list1: list, list2: list) -> bool:
-    'checcks the intersection of two lests'
+    'checks the intersection of two lests'
     for el in list1:
         if el in list2:
             return True
